f3f9fe3e660afcee1776e50f0850637c68995fe2
Luca Giussani <lucagiussani@horizenlabs.io>
2025-09-18 18:24:30 +0200

    Change ERC20 name from VfyOFT to zkVerify

--- deploy/VfyOFT.ts
+++ deploy/VfyOFT.ts
@@ -36,7 +36,7 @@ const deploy: DeployFunction = async (hre) => {
     const { address } = await deploy(contractName, {
         from: deployer,
         args: [
-            'VfyOFT', // name
+            'zkVerify', // name
             'VFY', // symbol
             endpointV2Deployment.address, // LayerZero's EndpointV2 address
             deployer, // owner
